0521e030e0fb6f0c9e6a842140c9a4810ad4ed28
ranvijay <ranvijay.cool9559@gmail.com>
2025-06-05 01:11:30 +0530

    Implement movie search functionality with API integration

---
+++ script.js
@@ -0,0 +1,46 @@
+const movieNameInput = document.getElementById("moviename-input");
+const searchBtn = document.getElementById("search-btn");
+const resultContainer = document.getElementById("result-container");
+
+const api = MOVIE_SEARCH_API;
+
+searchBtn.addEventListener("click", handleSearch);
+
+function handleSearch() {
+  resultContainer.innerHTML = "";
+
+  const movieName = movieNameInput.value.trim();
+
+  if(!movieName){
+    resultContainer.innerHTML = "<p>Please Enter a Movie Name.</p>";
+    return;
+  }
+
+  const url = `https://www.omdbapi.com/?apikey=${api}&s=${movieName}`;
+
+  fetch(url)
+  .then(
+    response => response.json()
+  )
+  .then(data => {
+    if(data.Response === "True"){
+      data.Search.forEach(movie => {
+        const poster = movie.Poster;
+        const title = movie.Title;
+
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+
+        movieCard.innerHTML = 
+        `
+          <img src= ${poster}>
+          <p>${title}</p>
+        `
+
+        resultContainer.appendChild(movieCard);
+      })
+    } else {
+      resultContainer.innerHTML = "<p>No Result Found.</p>"
+    }
+  })
+}
