5fad07b90de360177210debfb5614b312d2a6716
ranvijay <ranvijay.cool9559@gmail.com>
2025-07-01 19:20:27 +0530

    Add result header to display searched and trending movies

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const searchBtn = document.getElementById("search-btn");
 const resultContainer = document.getElementById("result-container");
 const suggestionContainer = document.getElementById("suggestion-container");
 const loading = document.getElementById("loading");
+const resultHeader = document.getElementById("result-header")
 
 const api = MOVIE_SEARCH_API;
+
+loadTrending();
 
 movieNameInput.addEventListener("input", debounce(handleSuggestion, 400));
 
@@ -83,6 +86,12 @@ function handleSearch() {
     return;
   }
 
+  suggestionContainer.style.display = "none";
+
+  resultHeader.innerHTML = `
+    <h2 style="text-align: center; margin-bottom: 20px;">Your Searched: ${movieName}</h2>
+  `;
+
   const url = `https://www.omdbapi.com/?apikey=${api}&s=${movieName}`;
 
 
@@ -127,4 +136,49 @@ function fetchImage(movie) {
   }
 
   return img;
+}
+
+function loadTrending() {
+  resultContainer.innerHTML = "";
+  suggestionContainer.innerHTML = "";
+  resultHeader.innerHTML = "";
+  loading.style.display = "block";
+
+  resultHeader.innerHTML = `
+    <h2 style="text-align: center; margin-bottom: 20px;">Trending Movies 🔥</h2>
+  `;
+
+  const trendingMovies = ["Avengers", "Inception", "Titanic", "Batman", "Interstellar"];
+
+  const fetchPromises = trendingMovies.map(movieName => {
+    const url = `https://www.omdbapi.com/?apikey=${api}&s=${movieName}`;
+    return fetch(url).then(res => res.json());
+  });
+
+  Promise.all(fetchPromises)
+    .then(allData => {
+      allData.forEach(data => {
+        if (data.Response === "True") {
+          data.Search.slice(0, 3).forEach(movie => {
+            const img = fetchImage(movie);
+
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("movie-card");
+
+            const title = document.createElement("p");
+            title.textContent = movie.Title;
+
+            movieCard.appendChild(img);
+            movieCard.appendChild(title);
+            resultContainer.appendChild(movieCard);
+          });
+        }
+      });
+
+      loading.style.display = "none";
+    })
+    .catch(error => {
+      resultContainer.innerHTML = `<p>Error fetching data: ${error.message}</p>`;
+      loading.style.display = "none";
+    });
 }
